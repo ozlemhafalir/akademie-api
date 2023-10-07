d6c9eb488d5a14f35c67c6e5d405faec202a3f18
Özlem Hafalır <ozlemoztep@gmail.com>
2023-10-07 11:37:11 +0200

    Add course views, serializers, urls

--- akademie/urls.py
+++ akademie/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('categories.urls'))
+    path('', include('categories.urls')),
+    path('', include('courses.urls')),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from courses.models import Course
+from courses.serializers import CourseSerializer
 
-# Create your views here.
+
+class CourseList(generics.ListCreateAPIView):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class CourseDetail(generics.RetrieveUpdateAPIView):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
